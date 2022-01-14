b981513bd7beb22f854d93c5c8148c3e89132518
Volodymyr Balashov <xaosssss@gmail.com>
2022-01-15 06:51:40 +0900

    completed with js

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,45 @@ const resetSectionColor = () => {
   section.style.backgroundColor = sectionDefaultColor;
 };
 
-if (notMobile) {
+const addButtonEvents = () => {
   cardsButton.forEach((button) => {
     button.addEventListener("mouseover", colorizeSection);
     button.addEventListener("mouseleave", resetSectionColor);
   });
+};
+
+const removeButtonEvents = () => {
+  cardsButton.forEach((button) => {
+    button.removeEventListener("mouseover", colorizeSection);
+    button.removeEventListener("mouseleave", resetSectionColor);
+  });
+};
+
+if (notMobile) {
+  addButtonEvents();
+}
+
+window.addEventListener(
+  "resize",
+  throttle(() => {
+    window.innerWidth < 960 ? removeButtonEvents() : addButtonEvents();
+  }, 50)
+);
+
+// This peace of code I found in internet.
+/* 
+  This function prevents my heavy event listeners
+  from being used for too many times 
+ */
+function throttle(callback, limit) {
+  var wait = false;
+  return function () {
+    if (!wait) {
+      callback.apply(null, arguments);
+      wait = true;
+      setTimeout(function () {
+        wait = false;
+      }, limit);
+    }
+  };
 }
