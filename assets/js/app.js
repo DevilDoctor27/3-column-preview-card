6f6e7d7d1f55ae822a888eb7789e5f45e158195a
DevilDoctor27 <xaosssss@gmail.com>
2022-01-14 20:00:49 +0900

    trying to remove mobile animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,21 @@ const section = document.getElementById("cards-section");
 const cardsButton = document.querySelectorAll(".card__button");
 const sectionDefaultColor = window.getComputedStyle(section).backgroundColor;
 
-cardsButton.forEach((button) => {
-  button.addEventListener("mouseover", () => {
-    section.style.backgroundColor = window.getComputedStyle(button).color;
-  });
-  button.addEventListener("mouseleave", () => {
-    section.style.backgroundColor = sectionDefaultColor;
-  });
+let notMobile = window.innerWidth >= 960;
+
+window.addEventListener("resize", () => {
+  console.log(window.innerWidth);
+  notMobile = window.innerWidth < 959;
+  return notMobile;
 });
+
+if (notMobile) {
+  cardsButton.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+      section.style.backgroundColor = window.getComputedStyle(button).color;
+    });
+    button.addEventListener("mouseleave", () => {
+      section.style.backgroundColor = sectionDefaultColor;
+    });
+  });
+}
